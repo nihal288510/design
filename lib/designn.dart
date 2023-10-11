0bf238ebb27f7ebaa47b436b896591b4f1d25412
nihal288510 <369al288510@gmail.com>
2023-10-11 12:25:57 +0530

    Initial commit

---
+++ lib/designn.dart
@@ -0,0 +1,39 @@
+// import 'package:flutter/material.dart';
+//
+// class Student extends StatefulWidget {
+//   const Student({super.key});
+//
+//   @override
+//   State<Student> createState() => _StudentState();
+// }
+//
+// class _StudentState extends State<Student> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.cyan,
+//       appBar: AppBar(
+//           backgroundColor: Colors.blueAccent,
+//           title: Center(child: Text('Welcome'))),
+//       body: Center(
+//           child: Container(
+//         height: 300,
+//         width: 300,
+//         color: Colors.amber,
+//         child: Center(
+//             child: Container(
+//           height: 200,
+//           width: 200,
+//           color: Colors.lightGreen,
+//           child: Center(
+//               child: Container(
+//             height: 100,
+//             width: 100,
+//             color: Colors.brown,
+//           )),
+//         )),
+//       )),
+//     );
+//   }
+// }
+//
---
+++ lib/niha.dart
@@ -0,0 +1,52 @@
+// import 'package:flutter/material.dart';
+//
+// class Student extends StatefulWidget {
+//   const Student({super.key});
+//
+//   @override
+//   State<Student> createState() => _StudentState();
+// }
+//
+// class _StudentState extends State<Student> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Column(
+//           children: [
+//             Column(
+//               children: [
+//                 Row(
+//                   children: [
+//                     Container(
+//                       width: 100,
+//                       height: 100,
+//                       color: Colors.purple,
+//                     ),Container(
+//                       width: 100,
+//                       height: 100,
+//                       color: Colors.red,
+//                     ),
+//                   ],
+//                 ),
+//               ],
+//             ),Row(
+//               children: [
+//                 Container(
+//                   width: 100,
+//                   height: 100,
+//                   color: Colors.yellowAccent,
+//                 ),Container(
+//                   width: 100,
+//                   height: 100,
+//                   color: Colors.greenAccent,
+//                 )
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//
+//     );
+//   }
+// }
